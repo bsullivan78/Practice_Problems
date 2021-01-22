8ab08314bf053811d67da3e1621dff8e5c087d8c
Brandon <brandonsullivan78@gmail.com>
2021-01-22 09:48:11 -0500

    Update
    Smoothed out some extra lines and removed some duplicates

--- unique.py
+++ unique.py
@@ -1,13 +1,12 @@
 
-unique_array = [0, 0, 0.55, 0, 0]
+unique_array = [0, 0, 0.55, 0, 0] #array of numbers goes here
 
 def find_uniq(arr):
-    a = arr[0]
     for i in range(1,len(arr)):
-        if(a == arr[i]):
+        if(arr[0] == arr[i]):
             t=0
         else:
             return arr[i]
-    return a
+    return arr[0]
 
 print(find_uniq(unique_array))
