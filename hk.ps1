1054bfe0d64268e971306239b59c411cb19cac8e
Brandon <brandonsullivan78@gmail.com>
2021-02-07 17:10:18 -0500

    Fun little practice problems for me to work on.
    Note: Ineffecient sort2 is currently somewhat right, but needs to be fixed.

---
+++ hk.ps1
@@ -0,0 +1,12 @@
+﻿$i = 0
+while($true){
+    $r = Get-Random -Minimum 0 -Maximum 2
+    $r2 = Get-Random -Minimum 0 -Maximum 2
+    $b = ($r -eq 0)
+    $b2 = ($r2 -eq 0)
+    if($b){Write-Host "Hiragana" -NoNewline}else{Write-Host "Katakana" -NoNewline}
+    if($b2){Write-Host ' "'}else{Write-Host ""}
+    if($i -gt 100){break}
+    $i++
+    Read-Host " "
+}
---
+++ inefficent_sort.ps1
@@ -0,0 +1,115 @@
+﻿#long sort
+#provided an array of numbers sort inefficently.
+
+function inefficent-sort1{
+    param(
+        $arr
+    )
+
+
+    Write-Host -NoNewline "Array: "
+    foreach($i in $arr){
+        Write-Host -NoNewline "$i,"
+    }
+    Write-Host ""
+    $t = 0
+    while($true){
+        $t++
+        $arr = $arr | Sort-Object { Get-Random }
+        
+        #check the array if in order
+        $ch = $true
+        foreach($i in 1..($arr.Count -1)){
+            if($ch){
+                $ch = ($arr[$i-1] -le $arr[$i])
+            }
+        }
+        if($ch){    #if in order
+            Write-Host -NoNewline "Sorted: "
+            foreach($i in $arr){
+                Write-Host -NoNewline "$i,"
+            }
+            Write-Host ""
+            Write-Host "Iterations: $t" 
+            break
+        }
+        if($t -ge (1000 * 1000)){     #if over a million
+            Write-Host -NoNewline "Array: "
+            foreach($i in $arr){
+                Write-Host -NoNewline "$i,"
+            }
+            Write-Host ""
+            Write-Host "**Failed to Iterate in Time**"
+            break
+        }
+    }
+}
+
+function inefficent-sort2{
+    param(
+        $arr
+    )
+
+    $t=0
+    while($true){
+        $t++
+        #if smaller than first put in first and move everything to right
+        #else contiune
+        $r = Get-Random -Minimum 1 -Maximum ($arr.Count-1)
+        if($arr[0] -le $arr[$r]){
+            $tp = $arr[0]
+            $arr[0] = $arr[$r]
+            
+            foreach($q in 2..($arr.Count-1)){
+                $tp2 = $arr[$q]
+                $arr[$q] = $arr[$q-1]
+                $arr[$q-1] = $tp2
+            }
+        } 
+
+
+
+        #check the array if in order
+        $ch = $true
+        foreach($i in 1..($arr.Count -1)){
+            if($ch){
+                $ch = ($arr[$i-1] -le $arr[$i])
+            }
+        }
+        if($ch){    #if in order
+            Write-Host -NoNewline "Sorted: "
+            foreach($i in $arr){
+                Write-Host -NoNewline "$i,"
+            }
+            Write-Host ""
+            Write-Host "Iterations: $t" 
+            break
+        }
+        if($t -ge (1000 * 1000)){     #if over a million
+            Write-Host -NoNewline "Array: "
+            foreach($i in $arr){
+                Write-Host -NoNewline "$i,"
+            }
+            Write-Host ""
+            Write-Host "**Failed to Iterate in Time**"
+            break
+        }
+    }
+}
+
+
+#-----------------------------------------------
+#intialize array
+$array = @(0)*8
+foreach($i in 0..($array.Count -1)){
+    $array[$i] = Get-Random -Maximum 100
+}
+#-----------------------------------------------
+
+inefficent-sort1 -arr $array
+
+Write-Host -NoNewline "Array: "
+foreach($i in $arr){
+    Write-Host -NoNewline "$i,"
+}
+Write-Host ""
